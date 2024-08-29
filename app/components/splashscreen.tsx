d1795ccebab1954604b63dce2995dd163626a1e5
Balaji P <balajiprakash2210@gmail.com>
2024-08-29 11:14:35 +0530

    fix splashscreen animation

--- app/components/splashscreen.tsx
+++ app/components/splashscreen.tsx
@@ -4,36 +4,37 @@ import Image from 'next/image';
 import { URL } from 'url';
 import style from '../styles/splash.module.css';
 import { useEffect, useState } from 'react';
-import anime from 'animejs/lib/anime.es.js';
 
 interface SplashProps {
   logoPath: URL['href'];
-  finishLoading: any;
+  finishLoading:  () => void;
 }
 
 const SplashScreen: React.FC<SplashProps> = (props) => {
   const { logoPath, finishLoading } = props;
   const [isMounted, setIsMounted] = useState(false);
-  const animate = () => {
-    const loader = anime.timeline({
-      complete: () => finishLoading(),
-    });
-    loader.add({
-      targets: '#logo',
-      delay: 0,
-      scale: 1,
-      duration: 1500,
-      easing: 'easeInOutExpo',
-    });
-  };
+  // const animate = () => {
+  //   const loader = anime.timeline({
+  //     complete: () => finishLoading(),
+  //   });
+  //   loader.add({
+  //     targets: '#logo',
+  //     delay: 0,
+  //     scale: 1,
+  //     duration: 1500,
+  //     easing: 'easeInOutExpo',
+  //   });
+  // };
 
   useEffect(() => {
     const timeOut = setTimeout(() => {
-      setIsMounted(true), 10;
-    });
-    animate();
+      setIsMounted(true);
+      finishLoading(); // This ensures finishLoading is called after the component is mounted.
+    }, 1000); // Set the timeout duration as needed
+
     return () => clearTimeout(timeOut);
-  }, []);
+  }, [finishLoading]);
+
   return (
     <div className={style.splash}>
       <Image id="logo" src={logoPath} alt="alt" width={142} height={46} />
